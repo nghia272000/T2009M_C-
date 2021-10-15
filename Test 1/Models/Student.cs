c1a869f049b705745ac3818e471a190a502d936e
nghia272000 <71159935+nghia272000@users.noreply.github.com>
2021-10-16 05:12:52 +0700

    Cm

---
+++ Test 1/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Test_1.Models
+{
+    public class Student
+    {
+        public string RollNumber { get; set; }
+        public string Fullname { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public StudentStatus Status { get; set; }
+        public int MyStatus { get; set; }
+
+        public override string ToString()
+        {
+            return
+                $"RollNumber {RollNumber}, Fullname {Fullname}, Birthday {Birthday}, Email {Email}, Phone{Phone}, Create At{CreatedAt}";
+        }
+    }
+}
---
+++ Test 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Test_1.Models;
+
+namespace Test_1
+{
+    public enum StudentStatus
+    {
+        Active =1,
+        Deactive =0,
+        Deleted = -1,
+            
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var st = new Student
+            {
+                RollNumber = "A001",
+                Birthday = DateTime.Parse("2000-07-02"),
+                Email = "blabla@gmail.com",
+                Phone = "0123456789",
+                CreatedAt = DateTime.Now,
+                Fullname = "Nghia Le",
+                Status = StudentStatus.Active,
+            };
+            Console.WriteLine((int)st.Status);    
+        }
+    }
+}
